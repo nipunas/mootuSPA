f3db26e4ce743433ddff6aa1bdc958b9bf38021e
nipunasilva <nipuna77@gmail.com>
2014-07-22 22:18:31 +0530

    Initial commit to develop branch with documentation

--- App/Helper/injector.js
+++ App/Helper/injector.js
@@ -2,6 +2,8 @@ define(function () {
     return {
         color: "black",
         size: "medium",
+        
+        //When an element is given clear the data inside it
         clearContent: function (element) {
             if (element.indexOf('#') == 0) {
                 $(element).empty();
